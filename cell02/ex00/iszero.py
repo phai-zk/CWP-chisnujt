2c94f074ba52e9b148be2159e4b690f21d781308
phai-zk <chisanupong.m34.1@gmail.com>
2026-09-22 15:13:45 +0700

    cell02

---
+++ cell02/ex00/iszero.py
@@ -0,0 +1,6 @@
+number = int(input())
+
+if (number == 0):
+    print("This number is equalto zero.")
+else:
+    print("This number is different from zero.")
---
+++ cell02/ex02/password.py
@@ -0,0 +1,7 @@
+password = "Python is awesome"
+input_password = input()
+
+if input_password == password:
+    print("ACCESS GRANTED")
+else:
+    print("ACCESS DENIED")
